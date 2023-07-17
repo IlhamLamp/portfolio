ccd5e154f6e10f2d529b93ed100cbb8410a8379c
IlhamLamp <ilhamutomo24@gmail.com>
2023-07-17 19:33:07 +0700

    add: toggle dark mode' '

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,6 +5,7 @@ import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai'
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
 import NavLogo from '../public/assets/navLogo.png'
+import ToggleTheme from './button/ToggleTheme'
 
 
 const Navbar = () => {
@@ -68,39 +69,36 @@ const Navbar = () => {
         </Link>
         <div>
           <ul className='hidden md:flex'>
-            <Link href='/'>
-              <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
-            </Link>
-            <Link href='/#about'>
-              <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
-            </Link>
-            <Link href='/#skills'>
-              <li className='ml-10 text-sm uppercase hover:border-b'>Skils</li>
-            </Link>
-            <Link href='/#projects'>
-              <li className='ml-10 text-sm uppercase hover:border-b'>Projects</li>
-            </Link>
-            <Link href='/#contact'>
-              <li className='ml-10 text-sm uppercase hover:border-b'>Contacts</li>
-            </Link>
-
-            <label htmlFor='toggleTheme' className="swap swap-rotate">
-              {/* this hidden checkbox controls the state */}
-              <input
-                type="checkbox"
-                id="toggleTheme"
-                checked={theme === "dark"}
-                onChange={toggleTheme}
-              />
-              {/* sun icon */}
-              <svg className="swap-on fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
-              {/* moon icon */}
-              <svg className="swap-off fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/></svg>
-            </label>
+            <div className='flex'>
+              <Link href='/'>
+                <li className='ml-10 text-sm hover:border-b'>Home</li>
+              </Link>
+              <Link href='/#about'>
+                <li className='ml-10 text-sm hover:border-b'>About</li>
+              </Link>
+              <Link href='/#skills'>
+                <li className='ml-10 text-sm hover:border-b'>Skils</li>
+              </Link>
+              <Link href='/#projects'>
+                <li className='ml-10 text-sm hover:border-b'>Projects</li>
+              </Link>
+              <Link href='/#contact'>
+                <li className='ml-10 text-sm hover:border-b'>Contacts</li>
+              </Link>
+            </div>
+            <div className='mx-4'>
+              <ToggleTheme theme={theme} toggleTheme={toggleTheme}/>
+            </div>
           </ul>
-          <div onClick={handleNav} className='md:hidden'>
-            <AiOutlineMenu size={25}/>
+
+          {/* Mode Device */}
+          <div className='md:hidden flex gap-3'>
+            <ToggleTheme theme={theme} toggleTheme={toggleTheme}/>
+            <div onClick={handleNav} className=''>
+              <AiOutlineMenu size={25}/>
+            </div>
           </div>
+
         </div>
       </div>
 
