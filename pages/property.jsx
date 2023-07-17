17096e9f2d9143c1f12d1f0d1788d74a90909a9e
IlhamLamp <ilhamutomo24@gmail.com>
2023-07-17 15:16:41 +0700

    add: dark mode toggle

---
+++ pages/property.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const property = () => {
+  return (
+    <div className='w-full'>
+        <div className='w-screen h-'>
+
+        </div>
+    </div>
+  )
+}
+
+export default property
