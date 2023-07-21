23ec630c08b5dd06c5c8fac80d3ba0d301a0fde1
IlhamLamp <ilhamutomo24@gmail.com>
2023-07-21 14:34:48 +0700

    styling projects card

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,14 +1,13 @@
-// import Image from 'next/image'
-// import Link from 'next/link'
 import React from 'react'
 import ProjectItem from './ProjectItem'
 import ProjectsData from '../data/projects.json'
+import '../styles/styles.css'
 
 const Projects = () => {
   return (
     <div id='projects' className='w-full'>
       <div className='max-w-[1240px] mx-auto px-2 py-16'>
-        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
+        <p className='text-xl tracking-widest uppercase text-primary textShadow'>
           Projects
         </p>
         <h2 className='py-4'>What I&apos;ve Built</h2>
