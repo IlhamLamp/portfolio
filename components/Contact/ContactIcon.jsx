ae139517f813e21332cca0064e719f80888ca6b5
IlhamLamp <ilhamutomo24@gmail.com>
2023-07-22 13:30:01 +0700

    fix ContactIcon component

--- components/Contact/ContactIcon.jsx
+++ components/Contact/ContactIcon.jsx
@@ -1,24 +1,45 @@
 import React from 'react'
 import Link from 'next/link';
 import { AiOutlineMail } from 'react-icons/ai';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
-import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { FaGithub, FaLinkedinIn, FaInstagram } from 'react-icons/fa';
 
 const ContactIcon = () => {
+
+    // EMAIL 
+    const emailAddress = 'memorest.me@gmail.com'
+    const subject = `Test subject`
+    const message = `Test message`
+
   return (
     <>
-    <div className='rounded-full shadow-lg bg-slate-50 shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-150 hover:shadow-none'>
-        <FaLinkedinIn className='text-bg'/>
-    </div>
-    <div className='rounded-full shadow-lg bg-slate-50 shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-150 hover:shadow-none'>
-        <FaGithub className='text-bg'/>
-    </div>
-    <div className='rounded-full shadow-lg bg-slate-50 shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-150 hover:shadow-none'>
-        <AiOutlineMail className='text-bg'/>
-    </div>
-    <div className='rounded-full shadow-lg bg-slate-50 shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-150 hover:shadow-none'>
-        <BsFillPersonLinesFill className='text-bg'/>
-    </div>
+    {/* Linkedin */}
+    <Link href="https://www.linkedin.com/in/ilham-nur-utomo-064b89220/" passHref>
+        <div className='rounded-full shadow-lg bg-slate-50 shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-150 hover:shadow-none'>
+            <FaLinkedinIn className='text-bg'/>
+        </div>
+    </Link>
+    {/* Github */}
+    <Link href="https://github.com/IlhamLamp" passHref>
+        <div className='rounded-full shadow-lg bg-slate-50 shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-150 hover:shadow-none'>
+            <FaGithub className='text-bg'/>
+        </div>
+    </Link>
+    {/* Email */}
+    <Link 
+        href={`
+            mailto:${emailAddress}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(message)}
+        `}
+    >
+        <div className='rounded-full shadow-lg bg-slate-50 shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-150 hover:shadow-none'>
+            <AiOutlineMail className='text-bg'/>
+        </div>
+    </Link>
+    {/* Instagram */}
+    <Link href="https://www.instagram.com/hamnurrr/" passHref>
+        <div className='rounded-full shadow-lg bg-slate-50 shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-150 hover:shadow-none'>
+            <FaInstagram className='text-bg'/>
+        </div>
+    </Link>
     </>
   )
 }
