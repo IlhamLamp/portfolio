9334df7c0c1070dd5858ef6d8fa4a3d7f00d96ed
IlhamLamp <ilhamutomo24@gmail.com>
2023-07-22 09:05:21 +0700

    fix demo

--- pages/projects/ProjectView.jsx
+++ pages/projects/ProjectView.jsx
@@ -56,7 +56,7 @@ const ProjectView = ({projectUrl}) => {
             <button className='px-8 py-2 mt-4 mr-8'>Code</button>
           </a>
           <a
-            href='https://fireclint.github.io/netflix-react-tailwind/'
+            href={demo}
             target='_blank'
             rel='noreferrer'
           >
