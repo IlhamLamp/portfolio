dfcbf0dce1597bc82841ce955622dd03665a02e4
IlhamLamp <ilhamutomo24@gmail.com>
2023-07-22 15:43:16 +0700

    fix logo and footer

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -83,8 +83,8 @@ const Navbar = () => {
           <Image 
             src={NavLogo}
             alt="/"
-            width='70'
-            height='50'
+            width='60'
+            height='60'
             className='cursor-pointer'
           />
         </Link>
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import Navbar from "@/components/Navbar"
-import Footer from "@/components/Footer"
 import '../styles/global.css'
 
 function MyApp({Component, pageProps}) {
@@ -7,7 +6,6 @@ function MyApp({Component, pageProps}) {
         <>
             <Navbar/>
             <Component {...pageProps}/>
-            <Footer/>
         </>
     )
 }
