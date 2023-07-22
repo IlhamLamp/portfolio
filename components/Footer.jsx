3bce4bad44e25dc7a993bce51d06ef169aca7b88
IlhamLamp <ilhamutomo24@gmail.com>
2023-07-22 14:56:39 +0700

    styling footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-gray-800 py-4 text-white text-center">
-      <p>&copy; {currentYear} Ilham. All rights reserved.</p>
+    <footer className="bg-whiteSec/5 py-4 text-white text-center">
+      <p><span className='text-whiteSec'>&copy;</span> {currentYear} <span className='text-primary textShadow'>Ilham.</span> All rights reserved.</p>
     </footer>
   );
 };
