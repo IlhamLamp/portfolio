da342de821b481c6247d67a5f2c89931d1f25d58
IlhamLamp <ilhamutomo24@gmail.com>
2023-07-22 10:09:15 +0700

    styling tech stack on preview projects

--- pages/projects/ProjectView.jsx
+++ pages/projects/ProjectView.jsx
@@ -1,9 +1,11 @@
-import Image from 'next/image';
-import React from 'react';
-import { RiRadioButtonFill } from 'react-icons/ri';
-import Link from 'next/link';
-import ProjectsButton from '@/components/Button/ProjectsButton';
+import Image from 'next/image'
+import React from 'react'
+import { RiRadioButtonFill } from 'react-icons/ri'
+import Link from 'next/link'
+import ProjectsButton from '@/components/Button/ProjectsButton'
 import projectsData from '../../data/projects.json'
+import '../../styles/styles.css'
+
 
 const ProjectView = ({projectUrl}) => {
     // Find the project based on the projectUrl
@@ -37,16 +39,16 @@ const ProjectView = ({projectUrl}) => {
           alt={title}
         />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
-          <h2 className='py-2'>{title}</h2>
+          <h2 className='py-2 whiteShadow'>{title}</h2>
           <h3 className='text-cursor'>{stack}</h3>
         </div>
       </div>
 
       <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8'>
         <div className='col-span-4'>
-          <p>Project</p>
-          <h2>Overview</h2>
-          <p>
+          <p className='text-white whiteShadow'>Project</p>
+          <h2 className='text-light textShadow'>Overview</h2>
+          <p className='text-whiteSec'>
             {description}
           </p>
           <a
@@ -64,12 +66,12 @@ const ProjectView = ({projectUrl}) => {
             <button className='px-8 py-2 mt-4'>Demo</button>
           </a>
         </div>
-        <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
+        <div className='col-span-4 md:col-span-1 shadow-xl backdrop-blur-md bg-white/10 shadow-gray-400/20 rounded-2xl py-4 hover:scale-105 ease-in-out duration-200'>
           <div className='p-2'>
-            <p className='text-center font-bold pb-2'>Technologies</p>
+            <p className='text-center font-bold pb-2 text-white'>Technologies</p>
             <div className='grid grid-cols-3 md:grid-cols-1'>
                 {tools.map((tool, index) => (
-                    <p key={index} className='text-secondary py-2 flex items-center'>
+                    <p key={index} className='text-whiteSec font-bold py-2 flex items-center'>
                         <RiRadioButtonFill className='pr-1 text-primary' />{tool}
                     </p>
                 ))}
