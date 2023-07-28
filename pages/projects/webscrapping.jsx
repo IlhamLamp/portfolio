47df70a4ff3b469fb1d6435427d388786dcc08a2
IlhamLamp <ilhamutomo24@gmail.com>
2023-07-28 10:19:14 +0700

    add: webscrapping projects

---
+++ pages/projects/webscrapping.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ProjectView from '../../components/ProjectView'
+
+const webscrapping = () => {
+    return <ProjectView projectUrl="/projects/webscrapping"/>
+}
+
+export default webscrapping
