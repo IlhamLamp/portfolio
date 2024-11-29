ada5b3e5d03c482544e800bcf2b369a13f371e98
IlhamLamp <ilhamutomo24@gmail.com>
2024-11-29 15:18:48 +0700

    fix: unexported comp

---
+++ pages/projects/communtity.jsx
@@ -0,0 +1,7 @@
+import ProjectView from "@/components/ProjectView";
+
+const communtityWeb = () => {
+  return <ProjectView projectUrl={"/projects/communtity"} />;
+};
+
+export default communtityWeb;
